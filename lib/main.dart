6646de0f47912d53da0a945b67ca527f9f43091b
Akim Svoboda <akim.svoboda@gmail.com>
2025-06-13 10:24:57 +0300

    Color yellow

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
       ),
-      home: const MyHomePage(title: 'Super Flutter Clicker Patch workflow test'),
+      home: const MyHomePage(title: 'Super clicker'),
     );
   }
 }
